1a44941118d4698f096e7e4b4b3b71538e6e602a
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2026-01-13 20:08:20 +0530

    object day 2 and string

---
+++ 13-1-2026/index.js
@@ -0,0 +1,77 @@
+//this keyword
+//add new properties
+//deleting properites
+//two obj comparisum
+
+// let obj = {
+//   name: "abc",
+//   myFun: function (a) {
+//     // console.log(this)
+//     console.log(this.name);
+//     return a + 10;
+//   },
+// };
+
+// console.log(obj.myFun(5));
+
+// obj.lastName = "xyz";
+// obj["age"] = 20;
+
+// obj.name="1234"
+
+// delete obj.name
+
+// let obj = {
+//   "employees": [
+//     {
+//       "firstName": "John",
+//       "lastName": "Doe"
+//     },
+//     {
+//       "firstName": "Anna",
+//       "lastName": "Smith"
+//     }
+//   ],
+//   "isActive": true
+// }
+
+// let obj2 = obj;
+
+// obj2.age = 20;
+
+// console.log(obj2);
+// console.log(obj);
+// console.log(obj.employees);
+
+//destructing
+// let {name, myFun} = obj
+// let a = 10
+
+// console.log(obj.name)
+// name = "123"
+// console.log(myFun)
+// console.log(obj)
+
+// ----------------------------------------------------------------/
+// String
+
+let str = "abc";
+
+// console.log(str[0])
+
+// for(let ele of str){
+//     console.log(ele)
+// }
+// let newStr = str+"h"
+// console.log(newStr)
+// console.log(str)
+
+// let arr = [1, "a", 20, "b"];
+// let allChar = str.split("")
+// allChar[0] = allChar[0].toLocaleUpperCase();
+// console.log(allChar.join(""))
+// let wordsInStr = str[0];
+// console.log(str);
+// console.log( wordsInStr.join("-"))
+
+//`${}`
