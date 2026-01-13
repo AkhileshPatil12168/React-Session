0cb45cef806865beaf7ab1afab49730a5c5f08e5
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2026-01-13 20:08:03 +0530

    object

---
+++ 12-1-2026/index.js
@@ -0,0 +1,32 @@
+//object
+
+// let obj = {
+//   name: "abc",
+//   age: 20,
+//   address: { city: "pune", pincode: 416004 },
+//   array: [1, 2, 3, 4],
+// };
+
+// console.log(obj.address);
+// console.log(obj["name"]);
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+//for in loop
+// variable  keyword   object
+// for (let key in obj) {
+//   console.log(key, obj[key]);
+// }
+
+// console.log(obj.array[2])
+
+// let obj = {
+//   name: "abc",
+//   age: 20,
+//   address: { city: "pune", pincode: 416004 },
+//   array: [1, 2, 3, 4],
+// };
+
+// //optional chaining
+// console.log(obj?.x)
