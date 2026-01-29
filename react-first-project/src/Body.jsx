3a481c2354fec79e3563cc10b757a77058d971b0
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2026-01-29 20:53:38 +0530

    onChnage, conditional rendering

--- react-first-project/src/Body.jsx
+++ react-first-project/src/Body.jsx
@@ -13,59 +13,108 @@ export function Abc() {
   );
 }
 
-function Body({ data }) {
-  //   let data = {};
-  //   const typeClass = `type-${data.type.toLowerCase()}`;
+function Body() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [type, setType] = useState("password");
+  let [bgColor, setBgColor] = useState("green");
 
-let [value, setValue]  = useState("click")
-
-  // let value = 10
-  const clickEvent = (e) => {
-    setValue("clicked")
+  const loginButton = () => {
+    alert(`${email}    ${password}`);
   };
-  console.log(value)
-
   return (
-    <>
-      {/* <div className="h-screen w-full grid grid-cols-2 grid-rows-3 gap-5">
-        <div className="bg-red-500 w-full">
-          <div className="bg-white size-full text-blue-500 text- font"></div>
-        </div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-        <div className="bg-green-500 w-full"></div>
-    
+    <div className={`h-screen  p-20`}>
+      <div className="flex gap-5 items-center">
+        <label>Email: </label>
+        <input
+          className="border-2"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
 
-      </div> */}
-      {/* <div className="grid grid-cols-3 p-10 gap-y-10">
-        {data?.map((obj, index) => (
-          <Card key={index} data={obj}></Card>
-        ))}
-      </div> */}
-
-      <div className="h-screen bg-red-100 p-20">
-        {/* <Button >
-          <p>click</p>
-        </Button> */}
-<p>{value}</p>
+        <label>Password: </label>
+        <input
+          className="border-2"
+          type={type}
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
         <button
-          // disabled = {true}
-          // hidden = {false}
-          name="abc"
-          value="123"
-          onClick={clickEvent}
-          className="px-4 py-2 rounded-lg font-semibold bg-blue-500"
+          onClick={() => {
+            type == "password" ? setType("text") : setType("password");
+            // bgColor == "red" ? setBgColor("green") : setBgColor("red");
+          }}
         >
-          click me
+          {type == "text" ? (
+            <svg
+              className="size-10"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5"
+                  stroke="#000000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>{" "}
+              </g>
+            </svg>
+          ) : (
+            <svg
+              className="size-10"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
+                  stroke="#000000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>{" "}
+                <path
+                  d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
+                  stroke="#000000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>{" "}
+              </g>
+            </svg>
+          )}
+        </button>
+
+        <button
+          className="bg-blue-500 px-5 py-2 rounded-md"
+          onClick={loginButton}
+        >
+          Login
         </button>
       </div>
-    </>
+      {/* <p>Name is : {show}</p> */}
+    </div>
   );
 }
 
